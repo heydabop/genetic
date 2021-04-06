b5e3205286e8f1922a7abe0106b35ed885762654
heydabop <heydabop@gmail.com>
2021-04-05 19:03:40 -0500

    implement neural network propagation

--- src/neural/mod.rs
+++ src/neural/mod.rs
@@ -1,14 +1,67 @@
-use rand::Rng;
+use rand::{
+    distributions::{Distribution, Uniform},
+    Rng,
+};
+use std::iter::Iterator;
+
+// A neural network where each neuron is stored as its bias and input weights
+// Due to this, the first layer doesn't really exist, at least in that it doesn't have neurons with a bias.
+// Instead we input raw values into the inputs of neurons in the second layer
 
 #[derive(Debug)]
 pub struct Neuron {
     bias: f32,
-    weights: Vec<f32>,
+    input_weights: Vec<f32>,
+}
+
+impl Neuron {
+    pub fn new(bias: f32, input_weights: Vec<f32>) -> Self {
+        assert!(!input_weights.is_empty());
+
+        Self {
+            bias,
+            input_weights,
+        }
+    }
+
+    pub fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert!(inputs.len() == self.input_weights.len());
+
+        // Sum up (input * weight) across the input synapses
+        let input = inputs
+            .iter()
+            .zip(self.input_weights.iter())
+            .map(|(i, w)| i * w)
+            .reduce(|a, b| a + b)
+            .unwrap();
+
+        // Add the neuron's bias and clamp negative values to 0.
+        (input + self.bias).max(0.0)
+    }
 }
 
 #[derive(Debug)]
 pub struct Layer {
     neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    pub fn new(neurons: Vec<Neuron>) -> Self {
+        assert!(!neurons.is_empty());
+        let num_weights = neurons[0].input_weights.len();
+
+        assert!(neurons.iter().all(|n| n.input_weights.len() == num_weights));
+
+        Self { neurons }
+    }
+
+    pub fn propagate(&self, inputs: &[f32]) -> Vec<f32> {
+        assert!(inputs.len() == self.neurons[0].input_weights.len());
+
+        // Feed the input values to each neuron in the layer and return their outputs
+
+        self.neurons.iter().map(|n| n.propagate(inputs)).collect()
+    }
 }
 
 #[derive(Debug)]
@@ -17,19 +70,85 @@ pub struct Network {
 }
 
 impl Network {
-    pub fn random<R: Rng + ?Sized>(rng: &mut R, neurons_per_layer: &[usize]) -> Self {
+    pub fn from_layers(layers: Vec<Layer>) -> Self {
+        assert!(!layers.is_empty());
+
+        Self { layers }
+    }
+
+    pub fn random<R: Rng + ?Sized>(mut rng: &mut R, neurons_per_layer: &[usize]) -> Self {
+        assert!(neurons_per_layer.len() > 1);
+
+        let range = Uniform::from(-1.0..1.0);
+
+        // The first layer is intentionally not created, and instead the output values of what would be those neurons is represented with `inputs` in the propagate call
         let layers: Vec<Layer> = neurons_per_layer
             .windows(2)
             .map(|n| Layer {
-                neurons: (0..n[0])
+                neurons: (0..n[1])
                     .map(|_| Neuron {
-                        bias: rng.gen_range(-1.0..=1.0),
-                        weights: (0..n[1]).map(|_| rng.gen_range(-1.0..=1.0)).collect(),
+                        bias: range.sample(&mut rng),
+                        input_weights: (0..n[0]).map(|_| range.sample(&mut rng)).collect(),
                     })
                     .collect(),
             })
             .collect();
 
         Self { layers }
+    }
+
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        assert_eq!(inputs.len(), self.layers[0].neurons[0].input_weights.len());
+
+        // Input the output of each layer into the next layer
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(&inputs))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn neuron_propagate() {
+        let neuron = Neuron::new(0.1, vec![0.3, 0.4, 0.6]);
+        assert!((neuron.propagate(&[-0.1, 0.7, 0.3]) - 0.53).abs() < f32::EPSILON);
+    }
+
+    #[test]
+    fn neuron_propagate_zero() {
+        let neuron = Neuron::new(-0.06, vec![0.6, 0.4, 0.5]);
+        assert!(neuron.propagate(&[-0.5, 0.3, 0.45]).abs() < f32::EPSILON);
+    }
+
+    #[test]
+    fn layer_propagate() {
+        let neurons = vec![
+            Neuron::new(0.6, vec![0.4, 0.6]),
+            Neuron::new(0.8, vec![0.2, 0.5]),
+            Neuron::new(-0.4, vec![0.7, 0.3]),
+        ];
+        let layer = Layer::new(neurons);
+        let outputs = layer.propagate(&[0.7, 0.1]);
+
+        assert!((outputs[0] - 0.94).abs() < f32::EPSILON);
+        assert!((outputs[1] - 0.99).abs() < f32::EPSILON);
+        assert!((outputs[2] - 0.12).abs() < f32::EPSILON);
+    }
+
+    #[test]
+    fn network_propagate() {
+        let network = Network::from_layers(vec![
+            Layer::new(vec![
+                Neuron::new(0.6, vec![0.4, 0.6]),
+                Neuron::new(0.8, vec![0.2, 0.5]),
+                Neuron::new(-0.4, vec![0.7, 0.3]),
+            ]),
+            Layer::new(vec![Neuron::new(0.5, vec![0.3, 0.4, 0.5])]),
+        ]);
+
+        assert!((network.propagate(vec![0.7, 0.1])[0] - 1.238).abs() < f32::EPSILON);
     }
 }
