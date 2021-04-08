53324b60cad7821607c8d8d507deee00390aa14b
heydabop <heydabop@gmail.com>
2021-04-07 23:16:59 -0500

    copy targets beyond edges during vision so agents can see through the wraparound

--- src/systems/vision.rs
+++ src/systems/vision.rs
@@ -1,4 +1,5 @@
 use crate::components::{Agent, Position, Target, Velocity};
+use crate::resources::MaxPos;
 use specs::{prelude::*, ReadStorage, System, WriteStorage};
 use std::f32::consts::PI;
 
@@ -10,27 +11,72 @@ struct DistanceAngle {
 pub struct Vision;
 
 impl<'a> System<'a> for Vision {
+    #[allow(clippy::type_complexity)]
     type SystemData = (
         WriteStorage<'a, Agent>,
         ReadStorage<'a, Position>,
         ReadStorage<'a, Velocity>,
         ReadStorage<'a, Target>,
+        ReadExpect<'a, MaxPos>,
     );
 
-    fn run(&mut self, (mut agents, positions, velocities, targets): Self::SystemData) {
+    fn run(&mut self, (mut agents, positions, velocities, targets, max): Self::SystemData) {
         let viewing_distance = 600.0; // distance that an agent can see a target
         let vision_cone = PI; // agent field of view in radians
         for (agent, agent_pos, agent_velocity) in (&mut agents, &positions, &velocities).join() {
+            let max = max.0;
             let num_receptors = agent.network.input_size();
             // individual vision receptor field of view
             let cone_slice = vision_cone / num_receptors as f32;
             // start and end of field of view
             let start = -vision_cone / 2.0;
             let end = vision_cone / 2.0;
-            // get distances and angles to all targets that are within agent's vision cone
-            let mut visible_targets: Vec<DistanceAngle> = (&positions, &targets)
+
+            // duplicate targets across boundaries so that agents can "see" to the other side of the word
+            let cloned_targets: Vec<Position> = (&positions, &targets)
                 .join()
-                .filter_map(|(target_pos, _)| {
+                .flat_map(|(&p, _)| {
+                    vec![
+                        p,
+                        Position {
+                            x: 0.0 - p.x,
+                            y: p.y,
+                        },
+                        Position {
+                            x: p.x,
+                            y: 0.0 - p.y,
+                        },
+                        Position {
+                            x: p.x + max.x,
+                            y: p.y,
+                        },
+                        Position {
+                            x: p.x,
+                            y: p.y + max.y,
+                        },
+                        Position {
+                            x: 0.0 - p.x,
+                            y: 0.0 - p.y,
+                        },
+                        Position {
+                            x: 0.0 - p.x,
+                            y: p.y + max.y,
+                        },
+                        Position {
+                            x: p.x + max.x,
+                            y: 0.0 - p.y,
+                        },
+                        Position {
+                            x: p.x + max.x,
+                            y: p.y + max.y,
+                        },
+                    ]
+                })
+                .collect();
+            // get distances and angles to all targets that are within agent's vision cone
+            let mut visible_targets: Vec<DistanceAngle> = cloned_targets
+                .iter()
+                .filter_map(|target_pos| {
                     let y_diff = target_pos.y - agent_pos.y;
                     let x_diff = target_pos.x - agent_pos.x;
                     let mut angle = y_diff.atan2(x_diff);
